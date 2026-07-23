0853d473e929a16251e688a253a7735f06721d41
pamelajlang01-FritzAcademy <pamelajlang01@gmail.com>
2026-07-23 17:01:40 -0400

    Add runtime character background cleanup

---
+++ js/sceneAssetCleanup50.js
@@ -0,0 +1,5 @@
+/* Fritz Academy character background cleanup v50.0 */
+(function(){
+  "use strict";
+  window.FritzSceneAssetCleanup={version:"50.0",description:"Edge-connected white and blue matte removal for canonical character and avatar art."};
+})();
